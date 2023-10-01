8d16a58442c5ae374f1cee2b9ae4e45efd93db5d
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-10-01 20:37:48 +0300

    Add debug logging to Axios instance

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,5 +1,26 @@
 import axios from 'axios';
+import { args } from '@/utils';
 
 import { BASE_URL } from '@/constants';
+import chalk from 'chalk';
 
 export const api = axios.create({ baseURL: BASE_URL });
+
+api.interceptors.request.use((req) => {
+  const reqDebugLog = `${chalk.bold(chalk.yellow(req.method?.toUpperCase()))} ${
+    req.baseURL
+  }${req.url}`;
+  if (args.debug) console.log(reqDebugLog, req.data || '');
+
+  return req;
+});
+api.interceptors.response.use((res) => {
+  const resDebugLog = `${chalk.bold(res.status)} ${res.statusText}`;
+  if (args.debug)
+    console.log(
+      res.status >= 400 ? chalk.red(resDebugLog) : chalk.green(resDebugLog),
+      res.data || ''
+    );
+
+  return res;
+});
