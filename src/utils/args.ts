e34337aeb06cf68000ac313cbe642706ce31a3a8
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-10-02 00:36:26 +0300

    Add missing args in ArgsOptions type

--- src/utils/args.ts
+++ src/utils/args.ts
@@ -7,17 +7,27 @@ export interface ArgsOptions {
    * Print help message for command/subcommand.
    */
   help?: boolean;
+  /** Shorthand for `--help`. */
+  h?: ArgsOptions['help'];
 
   /**
    * Minecraft version for downloaded files.
    * If not given, the latest version will be used.
    */
   version?: DatapacksMCVersion;
+  /** Shorthand for `--version`. */
+  v?: ArgsOptions['version'];
   /**
-   * Directory where files will be downloaded.
+   * Directory where file(s) will be downloaded.
    * If not given, the current working directory will be used.
    */
   outDir?: string;
+  /** Shorthand for `--outDir`. */
+  o?: ArgsOptions['outDir'];
+  /**
+   * Save a single zip file instead of multiple files.
+   */
+  noUnzip?: boolean;
 
   /**
    * Print error stack traces & additional debug logging.
