6471d9e4d171614e473d6cc01d4f476a17d4308e
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-10-02 21:31:59 +0300

    Reorder general consts

--- src/constants/general.ts
+++ src/constants/general.ts
@@ -2,8 +2,8 @@ import chalk from 'chalk';
 
 export const EXEC_NAME = 'vanillatweaks';
 
-export const INCORRECT_USAGE_MSG = 'Incorrect usage.';
-export const POSSIBLE_SUBCOMMANDS_MSG = chalk.bold(
+export const POSSIBLE_SUBCOMMANDS_MSG = `${chalk.bold(
   'Possible subcommands for %command:'
-);
+)}`;
+export const INCORRECT_USAGE_MSG = 'Incorrect usage.';
 export const INVALID_SUBCOMMAND_MSG = `'%subcommand' is not a valid subcommand for %command.`;
