a79314d89ebc9575afd96bf5a745c6de571b2ec5
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-10-02 21:32:46 +0300

    Add new line in POSSIBLE_SUBCOMMANDS_MSG const

--- src/constants/general.ts
+++ src/constants/general.ts
@@ -4,6 +4,7 @@ export const EXEC_NAME = 'vanillatweaks';
 
 export const POSSIBLE_SUBCOMMANDS_MSG = `${chalk.bold(
   'Possible subcommands for %command:'
-)}`;
+)}
+`;
 export const INCORRECT_USAGE_MSG = 'Incorrect usage.';
 export const INVALID_SUBCOMMAND_MSG = `'%subcommand' is not a valid subcommand for %command.`;
