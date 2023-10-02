bd2dddc7141f830b3e9187476f6d654213d51f10
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-10-02 22:05:02 +0300

    Fix axios instance not debug logging on errors

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,4 +1,8 @@
-import axios from 'axios';
+import axios, {
+  InternalAxiosRequestConfig,
+  isAxiosError,
+  type AxiosResponse,
+} from 'axios';
 import chalk from 'chalk';
 
 import { args } from '@/utils/args';
@@ -6,21 +10,33 @@ import { BASE_URL } from '@/constants/api';
 
 export const api = axios.create({ baseURL: BASE_URL });
 
-api.interceptors.request.use((req) => {
+const reqInterceptor = (req: InternalAxiosRequestConfig) => {
   const reqDebugLog = `${chalk.bold.yellow(req.method?.toUpperCase())} ${
     req.baseURL
   }${req.url}`;
   if (args.debug) console.log(reqDebugLog, req.data || '');
 
   return req;
-});
-api.interceptors.response.use((res) => {
-  const resDebugLog = `${chalk.bold(res.status)} ${res.statusText}`;
-  if (args.debug)
-    console.log(
-      res.status >= 400 ? chalk.red(resDebugLog) : chalk.green(resDebugLog),
-      res.data || ''
-    );
+};
+
+const resSuccessInterceptor = (res: AxiosResponse) => {
+  const resStatusLog = chalk.green(
+    `${chalk.bold(res.status)} ${res.statusText}`
+  );
+  if (args.debug) console.log(resStatusLog, res.data || '');
 
   return res;
-});
+};
+const resErrorInterceptor = (err: unknown) => {
+  if (!isAxiosError(err) || !err.response) throw err;
+
+  const resStatusLog = chalk.red(
+    `${chalk.bold(err.response.status)} ${err.response.statusText}`
+  );
+  if (args.debug) console.log(resStatusLog, err.response.data || '');
+
+  throw err;
+};
+
+api.interceptors.request.use(reqInterceptor);
+api.interceptors.response.use(resSuccessInterceptor, resErrorInterceptor);
