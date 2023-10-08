9a2928f57d85377eb1ff175acb17b86e7ec7cdad
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-10-08 15:46:29 +0300

    Add `toKebabCase` util

---
+++ src/utils/string.ts
@@ -0,0 +1,11 @@
+/**
+ * Converts a given string to kebab-case.
+ * @param str - The string to be converted.
+ */
+export const toKebabCase = (str: string) =>
+  str
+    ?.match(
+      /[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g
+    )
+    ?.map((x) => x.toLowerCase())
+    ?.join('-') || str;
