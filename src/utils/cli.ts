0fe91e0a4ce84fbdfa8e208b6e5fc0d4bed3aeb0
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-10-08 15:47:16 +0300

    Add general function to print packs array

--- src/utils/cli.ts
+++ src/utils/cli.ts
@@ -1,6 +1,9 @@
 import chalk from 'chalk';
 
+import { args } from './args';
+import { toKebabCase } from './string';
 import { POSSIBLE_SUBCOMMANDS_MSG } from '@/constants/general';
+import type { Pack } from '@/types/api';
 import type { CliSubcommand, ReadonlyCliSubcommand } from '@/types/cli';
 
 export const getSubcommandHelpMsg = ({
@@ -34,3 +37,23 @@ export const getCommandHelpMsg = (
 ) =>
   `${POSSIBLE_SUBCOMMANDS_MSG.replace('%command', command)}
 ${subcommands.map(getSubcommandHelpMsg).join('\n\n')}`;
+
+export const printPackList = (packs: Pack[]) =>
+  console.log(
+    args.detailed
+      ? packs
+          .map(
+            ({ name, display, version, description, incompatible }) =>
+              `${chalk.bold(
+                `${chalk.yellow(toKebabCase(name))}: ${display} v${version}`
+              )}\n${description}${
+                incompatible.length > 0
+                  ? `\n${chalk.red('Incompatible with:')} ${incompatible
+                      .map((incompatibleName) => toKebabCase(incompatibleName))
+                      .join(', ')}`
+                  : ''
+              }`
+          )
+          .join('\n\n')
+      : packs.map(({ name }) => toKebabCase(name)).join('\n')
+  );
