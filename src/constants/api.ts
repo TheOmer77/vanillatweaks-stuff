4310de6c1a19e4b4354f9f2557a63cddf5045c4d
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-10-08 15:29:50 +0300

    Add constants for other packs categories URLs
    Add API constants for resource packs and crafting tweaks categories URLs

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -2,5 +2,12 @@ export const BASE_URL = 'https://vanillatweaks.net';
 
 export const DOWNLOAD_URL = '/download/%filename';
 
-export const DATAPACKS_CATEGORIES_URL = `/assets/resources/json/%version/dpcategories.json`;
-export const DATAPACKS_ZIP_URL = `/assets/server/zipdatapacks.php`;
+export const RESOURCEPACKS_CATEGORIES_URL =
+  '/assets/resources/json/%version/rpcategories.json';
+
+export const DATAPACKS_CATEGORIES_URL =
+  '/assets/resources/json/%version/dpcategories.json';
+export const DATAPACKS_ZIP_URL = '/assets/server/zipdatapacks.php';
+
+export const CRAFTINGTWEAKS_CATEGORIES_URL =
+  '/assets/resources/json/%version/ctcategories.json';
