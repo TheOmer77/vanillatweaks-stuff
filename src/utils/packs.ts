cb7541bd3ce0d9f9b07283d374001e05142f798a
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-10-09 18:22:20 +0300

    Make `packListFromCategories` recursive
    When fetching resource packs, some categories may have categories within
    them; As such this function needs to be recursive to return a flat list
    with no packs missing.

--- src/utils/packs.ts
+++ src/utils/packs.ts
@@ -1,6 +1,15 @@
 import type { Pack, PacksCategory } from '@/types/api';
 
-export const packListFromCategories = (categories: PacksCategory[]) =>
+export const packListFromCategories = (categories: PacksCategory[]): Pack[] =>
   categories
-    .reduce((arr, { packs }) => [...arr, ...packs], [] as Pack[])
+    .reduce(
+      (arr, { packs, categories }) => [
+        ...arr,
+        ...packs,
+        ...(Array.isArray(categories)
+          ? packListFromCategories(categories)
+          : []),
+      ],
+      [] as Pack[]
+    )
     .sort((a, b) => (a.name > b.name ? 1 : a.name < b.name ? -1 : 0));
