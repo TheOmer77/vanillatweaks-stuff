50d60637737d763128187b51c40ee6244528bbb5
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-10-09 23:40:27 +0300

    Add resourcepacks help & messages consts

--- src/constants/resourcePacks.ts
+++ src/constants/resourcePacks.ts
@@ -1,3 +1,66 @@
+//#region Defaults
+
+import { getCommandHelpMsg, getSubcommandHelpMsg } from '@/utils/cli';
+import { EXEC_NAME } from './general';
+import { DEFAULT_MC_VERSION } from './versions';
+import type { ReadonlyCliSubcommand } from '@/types/cli';
+
+export const RESOURCEPACKS_ZIP_DEFAULT_NAME = 'vanillatweaks-resourcepacks.zip';
+
+//#endregion
+
+//#region Subcommands & usage
+
 export const RESOURCEPACKS_COMMAND = 'resourcepacks';
+export const RESOURCEPACKS_SUBCOMMANDS = [
+  {
+    id: 'list',
+    description: `List all available resource packs.`,
+    usage: `${EXEC_NAME} ${RESOURCEPACKS_COMMAND} list [OPTIONS]`,
+    options: [
+      {
+        args: ['version', 'v'],
+        description: `Minecraft version for downloaded files. (Default: ${DEFAULT_MC_VERSION})`,
+      },
+      {
+        args: ['detailed'],
+        description:
+          'Print list with additional details, such as descriptions and incompatible packs.',
+      },
+    ],
+  },
+  {
+    id: 'download',
+    description: `Download resource packs.`,
+    usage: `${EXEC_NAME} ${RESOURCEPACKS_COMMAND} download [OPTIONS] PACK_IDS...`,
+    options: [
+      {
+        args: ['version', 'v'],
+        description: `Minecraft version for downloaded files. (Default: ${DEFAULT_MC_VERSION})`,
+      },
+      {
+        args: ['outDir', 'o'],
+        description:
+          'Directory where file(s) will be downloaded. (Default: current directory)',
+      },
+    ],
+  },
+] as const satisfies readonly ReadonlyCliSubcommand[];
+
+//#endregion
+
+//#region Messages
 
 export const RESOURCEPACKS_RESOURCE_NAME = 'resource pack';
+export const RESOURCEPACKS_HELP_MSG = getCommandHelpMsg(
+  RESOURCEPACKS_COMMAND,
+  RESOURCEPACKS_SUBCOMMANDS
+);
+export const RESOURCEPACKS_LIST_HELP_MSG = getSubcommandHelpMsg(
+    RESOURCEPACKS_SUBCOMMANDS[0]
+  ),
+  RESOURCEPACKS_DOWNLOAD_HELP_MSG = getSubcommandHelpMsg(
+    RESOURCEPACKS_SUBCOMMANDS[1]
+  );
+
+//#endregion
