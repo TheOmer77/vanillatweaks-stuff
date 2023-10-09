6aad1e5fb8cad8af9eb10502741387b11103ab46
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-10-09 14:13:08 +0300

    General consts export syntax tweaks

--- src/constants/general.ts
+++ src/constants/general.ts
@@ -15,22 +15,20 @@ export const INVALID_RESOURCE_VERSION_MSG =
   '%resources are not available for Minecraft %version.';
 
 export const NONEXISTENT_PACKS_SINGLE_MSG = chalk.bold.yellow(
-  "The %resource '%packs' does not exist."
-);
-export const NONEXISTENT_PACKS_MULTIPLE_MSG = `${chalk.bold.yellow(
-  'The following %resources do not exist: '
-)}%packs`;
+    "The %resource '%packs' does not exist."
+  ),
+  NONEXISTENT_PACKS_MULTIPLE_MSG = `${chalk.bold.yellow(
+    'The following %resources do not exist: '
+  )}%packs`;
 export const INVALID_PACK_IDS_MSG = 'All pack IDs given are invalid.';
 export const INCOMPATIBLE_PACKS_MSG =
   'The following crafting tweaks are incompatible with each other: %packs';
-export const DOWNLOADING_PACKS_SINGLE_MSG = 'Downloading %resource: %packs';
-export const DOWNLOADING_PACKS_MULTIPLE_MSG =
-  'Downloading %count %resources: %packs';
+export const DOWNLOADING_PACKS_SINGLE_MSG = 'Downloading %resource: %packs',
+  DOWNLOADING_PACKS_MULTIPLE_MSG = 'Downloading %count %resources: %packs';
 export const DOWNLOAD_SUCCESS_SINGLE_MSG =
-  'Successfully downloaded %count %resource to %path.';
-export const DOWNLOAD_SUCCESS_MULTIPLE_MSG =
-  'Successfully downloaded %count %resources to %path.';
+    'Successfully downloaded %count %resource to %path.',
+  DOWNLOAD_SUCCESS_MULTIPLE_MSG =
+    'Successfully downloaded %count %resources to %path.';
 export const DOWNLOAD_FAIL_SINGLE_MSG =
-  'Failed to download %count %resource to %path.';
-export const DOWNLOAD_FAIL_MULTIPLE_MSG =
-  'Failed to download %count %resources to %path.';
+    'Failed to download %count %resource to %path.',
+  DOWNLOAD_FAIL_MULTIPLE_MSG = 'Failed to download %count %resources to %path.';
