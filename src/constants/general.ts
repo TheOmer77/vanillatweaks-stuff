dd0f381c2699c9c2cb9cc4f69f1ddfead7f0ca01
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-10-10 14:59:10 +0300

    Sort general constants into sections

--- src/constants/general.ts
+++ src/constants/general.ts
@@ -1,19 +1,44 @@
+//#region Exec name
+
 export const EXEC_NAME = 'vanillatweaks';
 
-export const POSSIBLE_SUBCOMMANDS_MSG = 'Possible subcommands for %command:\n';
+//#endregion
+
+//#region Help messages
+
+export const USAGE_PREFIX_MSG = `Usage: `;
+export const USAGE_MAIN_MSG = `${EXEC_NAME} %command <COMMAND>`;
+export const USAGE_COMMAND_MSG = `${EXEC_NAME} %command <COMMAND>`;
+export const USAGE_SUBCOMMAND_MSG = `${EXEC_NAME} %command %subcommand %usage`;
+export const USAGE_COMMANDS_PREFIX_MSG = `Commands: `;
+export const USAGE_OPTIONS_PREFIX_MSG = `Options: `;
+
+//#endregion
+
+//#region Usage error messages
+
 export const INCORRECT_USAGE_MSG = 'Incorrect usage.';
 export const INVALID_SUBCOMMAND_MSG =
   "'%subcommand' is not a valid subcommand for %command.";
 export const INVALID_VERSION_MSG =
   "'%version' is not a valid version for Vanilla Tweaks packs.";
+
+//#endregion
+
+//#region Packs error messages
+
 export const INVALID_RESOURCE_VERSION_MSG =
   '%resources are not available for Minecraft %version.';
-
 export const NONEXISTENT_SINGLE_MSG = "The %resource '%packs' does not exist.",
   NONEXISTENT_MULTIPLE_MSG = 'The following %resources do not exist: ';
 export const INVALID_PACK_IDS_MSG = 'All pack IDs given are invalid.';
 export const INCOMPATIBLE_PACKS_MSG =
   'The following %resources are incompatible with each other: %packs';
+
+//#endregion
+
+//#region Download messages
+
 export const DOWNLOADING_SINGLE_MSG = 'Downloading %resource: %packs',
   DOWNLOADING_MULTIPLE_MSG = 'Downloading %count %resources: %packs';
 export const DOWNLOAD_SUCCESS_SINGLE_MSG =
@@ -23,3 +48,5 @@ export const DOWNLOAD_SUCCESS_SINGLE_MSG =
 export const DOWNLOAD_FAIL_SINGLE_MSG =
     'Failed to download %count %resource to %path.',
   DOWNLOAD_FAIL_MULTIPLE_MSG = 'Failed to download %count %resources to %path.';
+
+//#endregion
