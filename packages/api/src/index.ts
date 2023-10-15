bcf9df0b1db3be7e8950f542e2e4fd92ec19164a
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-10-15 14:22:28 +0300

    Add error handler in rest api package

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,7 +1,37 @@
-import { Elysia } from 'elysia';
+import { Elysia, ErrorHandler } from 'elysia';
 import router from './routes';
 
+const errorHandler: ErrorHandler = ({
+  error,
+  set,
+  code,
+  path,
+  request: { method },
+}) => {
+  switch (code) {
+    case 'VALIDATION':
+      set.status = 400;
+      break;
+    case 'PARSE':
+    case 'INVALID_COOKIE_SIGNATURE':
+      set.status = 401;
+      break;
+    case 'NOT_FOUND':
+      set.status = 404;
+      break;
+    default:
+      set.status = 500;
+  }
+
+  console.error(method, path, set.status);
+  error.stack && console.error(error.stack);
+
+  return { success: false, message: error.message };
+};
+
 const app = new Elysia();
+
+app.onError(errorHandler);
 
 app.use(router);
 
