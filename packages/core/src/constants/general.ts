af04ed46bd95cf8d03f2c41c431f9da84301240f
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-10-15 22:19:51 +0300

    Move some CLI only messages to CLI consts

--- packages/core/src/constants/general.ts
+++ packages/core/src/constants/general.ts
@@ -28,17 +28,3 @@ export const INCOMPATIBLE_PACKS_MSG =
   'The following %resources are incompatible with each other: %packs';
 
 //#endregion
-
-//#region Download messages
-
-export const DOWNLOADING_SINGLE_MSG = 'Downloading %resource: %packs',
-  DOWNLOADING_MULTIPLE_MSG = 'Downloading %count %resources: %packs';
-export const DOWNLOAD_SUCCESS_SINGLE_MSG =
-    'Successfully downloaded %count %resource to %path.',
-  DOWNLOAD_SUCCESS_MULTIPLE_MSG =
-    'Successfully downloaded %count %resources to %path.';
-export const DOWNLOAD_FAIL_SINGLE_MSG =
-    'Failed to download %count %resource to %path.',
-  DOWNLOAD_FAIL_MULTIPLE_MSG = 'Failed to download %count %resources to %path.';
-
-//#endregion
