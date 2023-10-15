0440ca3de7547c6f2561be4a630307b9a19e3c4e
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-10-15 22:13:03 +0300

    Add API route to download a single datapack zip

--- packages/api/src/routes/datapacks.ts
+++ packages/api/src/routes/datapacks.ts
@@ -1,9 +1,17 @@
-import { Elysia } from 'elysia';
+import { Elysia, NotFoundError } from 'elysia';
+import AdmZip from 'adm-zip';
 
 import {
+  DATAPACKS_RESOURCE_NAME,
+  NONEXISTENT_SINGLE_MSG,
+  downloadFile,
   formatPacksList,
   getDatapacksCategories,
+  getDatapacksZipLink,
+  getPacksByCategory,
+  getZipEntryData,
   packListFromCategories,
+  stringSubst,
 } from 'core';
 import { getPacksHook } from '../hooks/packs';
 
@@ -18,6 +26,57 @@ datapacksRouter.get(
   getPacksHook
 );
 
-datapacksRouter.get('/:packId', () => {});
+datapacksRouter.get(
+  '/:packId',
+  async ({ params: { packId }, query: { version } }) => {
+    const categories = await getDatapacksCategories(version),
+      packList = formatPacksList(packListFromCategories(categories)),
+      selectedPack = packList.find(({ id }) => id === packId);
+
+    if (!selectedPack)
+      throw new NotFoundError(
+        stringSubst(NONEXISTENT_SINGLE_MSG, {
+          resource: DATAPACKS_RESOURCE_NAME,
+          packs: packId,
+        })
+      );
+
+    const packsByCategory = getPacksByCategory([packId], categories);
+
+    const zipFilename = (await getDatapacksZipLink(version, packsByCategory))
+        .split('/')
+        .at(-1) as string,
+      zipBuffer = await downloadFile(zipFilename);
+
+    const zip = new AdmZip(zipBuffer),
+      zipEntries = zip
+        .getEntries()
+        .sort((a, b) => (a.name > b.name ? 1 : a.name < b.name ? -1 : 0));
+
+    const packFile = (
+      (
+        await Promise.allSettled(
+          zipEntries.map(async (entry) => await getZipEntryData(entry))
+        )
+      ).filter(
+        ({ status }) => status === 'fulfilled'
+      ) as PromiseFulfilledResult<Buffer>[]
+    )?.[0]?.value;
+
+    if (!packFile)
+      throw new Error(
+        // TODO: Move this messaged to consts
+        stringSubst("Failed to download %resource '%packId'.", {
+          resource: DATAPACKS_RESOURCE_NAME,
+          packId,
+        })
+      );
+
+    return new Response(packFile, {
+      headers: { 'Content-Type': `attachment; filename=${packId}.zip` },
+    });
+  },
+  getPacksHook
+);
 
 export default datapacksRouter;
