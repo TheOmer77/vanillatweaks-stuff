fec47d7d55e327c1770095c8e8293b004aaa7fdc
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-10-16 14:30:34 +0300

    Add zip utils in core, using JSZip

--- packages/core/src/utils/zip.ts
+++ packages/core/src/utils/zip.ts
@@ -1,4 +1,25 @@
+import JSZip, { JSZipObject } from 'jszip';
+
+// TODO: REMOVE ADM-ZIP
 import type { IZipEntry } from 'adm-zip';
+
+export const zipFromBuffer = async (buffer: Buffer) =>
+  await JSZip.loadAsync(buffer);
+
+export const bufferFromZip = async (zip: JSZip) =>
+  Buffer.from(await zip.generateAsync({ type: 'arraybuffer' }));
+
+export const modifiedZipFromBuffer = async (
+  buffer: Buffer,
+  modifyZip: (zip: JSZip) => void
+) => {
+  const zip = await zipFromBuffer(buffer);
+  modifyZip(zip);
+  return await bufferFromZip(zip);
+};
+
+export const getZipFile = async (zipFile: JSZipObject) =>
+  Buffer.from(await zipFile.async('arraybuffer'));
 
 export const getZipEntryData = async (entry: IZipEntry) =>
   await new Promise<Buffer>((resolve, reject) => {
