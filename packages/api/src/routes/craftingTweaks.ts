eab7275d813f52314d582c54ba39aac9f5e24ca6
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-10-16 15:11:54 +0300

    Add API route to download single crafting tweak zip

--- packages/api/src/routes/craftingTweaks.ts
+++ packages/api/src/routes/craftingTweaks.ts
@@ -1,10 +1,21 @@
-import { Elysia } from 'elysia';
+import { Elysia, NotFoundError } from 'elysia';
 import {
+  CRAFTINGTWEAKS_ICON_URL,
+  CRAFTINGTWEAKS_RESOURCE_NAME,
+  DEFAULT_MC_VERSION,
+  DOWNLOAD_PACKS_URL,
+  NONEXISTENT_SINGLE_MSG,
+  downloadFile,
   formatPacksList,
   getCraftingTweaksCategories,
+  getCraftingTweaksZipLink,
+  getPacksByCategory,
+  modifiedZipFromBuffer,
   packListFromCategories,
+  stringSubst,
 } from 'core';
 import { getPacksHook } from '../hooks/packs';
+import { DOWNLOAD_FAIL_SINGLE_MSG } from '../constants/general';
 
 const craftingTweaksRouter = new Elysia();
 
@@ -14,6 +25,62 @@ craftingTweaksRouter.get(
     formatPacksList(
       packListFromCategories(await getCraftingTweaksCategories(version))
     ),
+  getPacksHook
+);
+
+craftingTweaksRouter.get(
+  '/:packId',
+  async ({ params: { packId }, query: { version = DEFAULT_MC_VERSION } }) => {
+    const categories = await getCraftingTweaksCategories(version),
+      packList = formatPacksList(packListFromCategories(categories)),
+      selectedPack = packList.find(({ id }) => id === packId);
+
+    if (!selectedPack)
+      throw new NotFoundError(
+        stringSubst(NONEXISTENT_SINGLE_MSG, {
+          resource: CRAFTINGTWEAKS_RESOURCE_NAME,
+          packs: packId,
+        })
+      );
+
+    const packsByCategory = getPacksByCategory([packId], categories);
+
+    const zipFilename = (
+      await getCraftingTweaksZipLink(version, packsByCategory)
+    )
+      .split('/')
+      .at(-1) as string;
+    const [zipBuffer, iconBuffer] = (
+      (await Promise.allSettled([
+        downloadFile(
+          stringSubst(DOWNLOAD_PACKS_URL, { filename: zipFilename })
+        ),
+        downloadFile(
+          stringSubst(CRAFTINGTWEAKS_ICON_URL, {
+            version,
+            pack: selectedPack.name,
+          })
+        ),
+      ])) as PromiseFulfilledResult<Buffer>[]
+    ).map((promise) => promise?.value);
+
+    if (!zipBuffer)
+      throw new Error(
+        stringSubst(DOWNLOAD_FAIL_SINGLE_MSG, {
+          resource: CRAFTINGTWEAKS_RESOURCE_NAME,
+          packId,
+        })
+      );
+
+    const modifiedZipBuffer = await modifiedZipFromBuffer(zipBuffer, (zip) => {
+      zip.remove('Selected Packs.txt');
+      if (iconBuffer) zip.file('pack.png', iconBuffer);
+    });
+
+    return new Response(modifiedZipBuffer, {
+      headers: { 'Content-Type': `attachment; filename=${packId}.zip` },
+    });
+  },
   getPacksHook
 );
 
--- packages/core/src/constants/api.ts
+++ packages/core/src/constants/api.ts
@@ -14,4 +14,6 @@ export const DATAPACKS_ZIP_URL = '/assets/server/zipdatapacks.php';
 
 export const CRAFTINGTWEAKS_CATEGORIES_URL =
   '/assets/resources/json/%version/ctcategories.json';
+export const CRAFTINGTWEAKS_ICON_URL =
+  '/assets/resources/icons/craftingtweaks/%version/%pack.png';
 export const CRAFTINGTWEAKS_ZIP_URL = '/assets/server/zipcraftingtweaks.php';
