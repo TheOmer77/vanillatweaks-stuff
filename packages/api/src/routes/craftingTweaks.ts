3d0d80e4f23c95b257de926f504c3f1c7195b8e0
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-10-17 13:41:34 +0300

    Use `downloadSinglePack` in craftingtweaks route

--- packages/api/src/routes/craftingTweaks.ts
+++ packages/api/src/routes/craftingTweaks.ts
@@ -1,24 +1,15 @@
 import { Elysia } from 'elysia';
+
 import {
-  CRAFTINGTWEAKS_ICON_URL,
-  CRAFTINGTWEAKS_RESOURCE_NAME,
   CRAFTINGTWEAKS_ZIP_DEFAULT_NAME,
   DEFAULT_MC_VERSION,
-  DOWNLOAD_PACKS_URL,
-  NONEXISTENT_SINGLE_MSG,
-  downloadFile,
+  downloadSinglePack,
   downloadZippedPacks,
   getCraftingTweaksCategories,
-  getCraftingTweaksZipLink,
-  getPacksByCategory,
-  HttpError,
-  modifiedZipFromBuffer,
   packListFromCategories,
   packListWithIds,
-  stringSubst,
 } from 'core';
 import { downloadPacksZipHook, getPacksHook } from '../hooks/packs';
-import { DOWNLOAD_FAIL_SINGLE_MSG } from '../constants/general';
 
 const craftingTweaksRouter = new Elysia();
 
@@ -53,55 +44,13 @@ craftingTweaksRouter.get(
 craftingTweaksRouter.get(
   '/:packId',
   async ({ params: { packId }, query: { version = DEFAULT_MC_VERSION } }) => {
-    const categories = await getCraftingTweaksCategories(version),
-      packList = packListWithIds(packListFromCategories(categories)),
-      selectedPack = packList.find(({ id }) => id === packId);
+    const zipBuffer = await downloadSinglePack(
+      'craftingTweak',
+      packId,
+      version
+    );
 
-    if (!selectedPack)
-      throw new HttpError(
-        stringSubst(NONEXISTENT_SINGLE_MSG, {
-          resource: CRAFTINGTWEAKS_RESOURCE_NAME,
-          packs: packId,
-        }),
-        404
-      );
-
-    const packsByCategory = getPacksByCategory([packId], categories);
-
-    const zipFilename = (
-      await getCraftingTweaksZipLink(version, packsByCategory)
-    )
-      .split('/')
-      .at(-1) as string;
-    const [zipBuffer, iconBuffer] = (
-      (await Promise.allSettled([
-        downloadFile(
-          stringSubst(DOWNLOAD_PACKS_URL, { filename: zipFilename })
-        ),
-        downloadFile(
-          stringSubst(CRAFTINGTWEAKS_ICON_URL, {
-            version,
-            pack: selectedPack.name,
-          })
-        ),
-      ])) as PromiseFulfilledResult<Buffer>[]
-    ).map((promise) => promise?.value);
-
-    if (!zipBuffer)
-      throw new HttpError(
-        stringSubst(DOWNLOAD_FAIL_SINGLE_MSG, {
-          resource: CRAFTINGTWEAKS_RESOURCE_NAME,
-          packId,
-        }),
-        500
-      );
-
-    const modifiedZipBuffer = await modifiedZipFromBuffer(zipBuffer, (zip) => {
-      zip.remove('Selected Packs.txt');
-      if (iconBuffer) zip.file('pack.png', iconBuffer);
-    });
-
-    return new Response(modifiedZipBuffer, {
+    return new Response(zipBuffer, {
       headers: { 'Content-Disposition': `attachment; filename=${packId}.zip` },
     });
   },
