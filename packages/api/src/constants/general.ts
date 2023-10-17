57af4a0d2f933519ed8ba978e4cb89798bc739c9
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-10-17 13:59:18 +0300

    Remove unused const

--- packages/api/src/constants/general.ts
+++ packages/api/src/constants/general.ts
@@ -1,5 +1,2 @@
 export const PACKS_QUERY_MISSING_MSG =
   "Query parameter 'packs' is required, and must be a comma-separated list of pack IDs.";
-
-export const DOWNLOAD_FAIL_SINGLE_MSG =
-  "Failed to download %resource '%packId'.";
