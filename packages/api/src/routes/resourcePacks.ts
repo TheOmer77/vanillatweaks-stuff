cfa00aae3c59fa824754048e9490f63ff5e3c63a
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-10-17 13:41:11 +0300

    Use `downloadSinglePack` in resourcepacks route

--- packages/api/src/routes/resourcePacks.ts
+++ packages/api/src/routes/resourcePacks.ts
@@ -2,24 +2,14 @@ import { Elysia } from 'elysia';
 
 import {
   DEFAULT_MC_VERSION,
-  DOWNLOAD_PACKS_URL,
-  NONEXISTENT_SINGLE_MSG,
-  RESOURCEPACKS_ICON_URL,
-  RESOURCEPACKS_RESOURCE_NAME,
   RESOURCEPACKS_ZIP_DEFAULT_NAME,
-  downloadFile,
   downloadZippedPacks,
-  getPacksByCategory,
   getResourcePacksCategories,
-  getResourcePacksZipLink,
-  HttpError,
-  modifiedZipFromBuffer,
   packListFromCategories,
   packListWithIds,
-  stringSubst,
+  downloadSinglePack,
 } from 'core';
 import { downloadPacksZipHook, getPacksHook } from '../hooks/packs';
-import { DOWNLOAD_FAIL_SINGLE_MSG } from '../constants/general';
 
 const resourcePacksRouter = new Elysia();
 
@@ -54,55 +44,9 @@ resourcePacksRouter.get(
 resourcePacksRouter.get(
   '/:packId',
   async ({ params: { packId }, query: { version = DEFAULT_MC_VERSION } }) => {
-    const categories = await getResourcePacksCategories(version),
-      packList = packListWithIds(packListFromCategories(categories)),
-      selectedPack = packList.find(({ id }) => id === packId);
+    const zipBuffer = await downloadSinglePack('resourcePack', packId, version);
 
-    if (!selectedPack)
-      throw new HttpError(
-        stringSubst(NONEXISTENT_SINGLE_MSG, {
-          resource: RESOURCEPACKS_RESOURCE_NAME,
-          packs: packId,
-        }),
-        404
-      );
-
-    const packsByCategory = getPacksByCategory([packId], categories);
-
-    const zipFilename = (
-      await getResourcePacksZipLink(version, packsByCategory)
-    )
-      .split('/')
-      .at(-1) as string;
-    const [zipBuffer, iconBuffer] = (
-      (await Promise.allSettled([
-        downloadFile(
-          stringSubst(DOWNLOAD_PACKS_URL, { filename: zipFilename })
-        ),
-        downloadFile(
-          stringSubst(RESOURCEPACKS_ICON_URL, {
-            version,
-            pack: selectedPack.name,
-          })
-        ),
-      ])) as PromiseFulfilledResult<Buffer>[]
-    ).map((promise) => promise?.value);
-
-    if (!zipBuffer)
-      throw new HttpError(
-        stringSubst(DOWNLOAD_FAIL_SINGLE_MSG, {
-          resource: RESOURCEPACKS_RESOURCE_NAME,
-          packId,
-        }),
-        500
-      );
-
-    const modifiedZipBuffer = await modifiedZipFromBuffer(zipBuffer, (zip) => {
-      zip.remove('Selected Packs.txt');
-      if (iconBuffer) zip.file('pack.png', iconBuffer);
-    });
-
-    return new Response(modifiedZipBuffer, {
+    return new Response(zipBuffer, {
       headers: { 'Content-Disposition': `attachment; filename=${packId}.zip` },
     });
   },
