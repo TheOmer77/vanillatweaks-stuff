e8391120b420d70997fc2fc5c673021088556df9
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-10-19 00:07:48 +0300

    Wrap CLI index.ts main code in a function
    Allow the CLI to run under Node 18...?

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -11,36 +11,40 @@ import { RESOURCEPACKS_COMMAND } from './constants/resourcePacks';
 import { DATAPACKS_COMMAND } from './constants/datapacks';
 import { CRAFTINGTWEAKS_COMMAND } from './constants/craftingTweaks';
 
-try {
-  switch (args._[0]) {
-    case RESOURCEPACKS_COMMAND:
-      await resourcePacks();
-      break;
-    case DATAPACKS_COMMAND:
-      await datapacks();
-      break;
-    case CRAFTINGTWEAKS_COMMAND:
-      await craftingTweaks();
-      break;
-    default:
-      console.log(getMainHelpMsg());
-      if (!(args.help || args.h)) {
-        console.log();
-        throw new Error(INCORRECT_USAGE_MSG);
-      }
+const main = async () => {
+  try {
+    switch (args._[0]) {
+      case RESOURCEPACKS_COMMAND:
+        await resourcePacks();
+        break;
+      case DATAPACKS_COMMAND:
+        await datapacks();
+        break;
+      case CRAFTINGTWEAKS_COMMAND:
+        await craftingTweaks();
+        break;
+      default:
+        console.log(getMainHelpMsg());
+        if (!(args.help || args.h)) {
+          console.log();
+          throw new Error(INCORRECT_USAGE_MSG);
+        }
+    }
+  } catch (err) {
+    const errMsg = err instanceof Error ? err.message : 'Unknown error!',
+      errStack =
+        err instanceof Error && err.stack
+          ? err.stack.split('\n').slice(1).join('\n')
+          : undefined;
+    console.error(
+      chalk.red(
+        `${chalk.bold('Error: ')}${chalk.reset(errMsg)}${
+          args.debug && errStack ? chalk.reset(`\n${errStack}`) : ''
+        }`
+      )
+    );
+    process.exit(1);
   }
-} catch (err) {
-  const errMsg = err instanceof Error ? err.message : 'Unknown error!',
-    errStack =
-      err instanceof Error && err.stack
-        ? err.stack.split('\n').slice(1).join('\n')
-        : undefined;
-  console.error(
-    chalk.red(
-      `${chalk.bold('Error: ')}${chalk.reset(errMsg)}${
-        args.debug && errStack ? chalk.reset(`\n${errStack}`) : ''
-      }`
-    )
-  );
-  process.exit(1);
-}
+};
+
+main();
